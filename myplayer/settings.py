1257a091d2e8aac8cc0e406e4a999d7845d20d13
Djama-Mo <djamamosss@gmail.com>
2021-08-18 01:15:47 +0300

    Installed 'direnv' to hide secret key of project

--- myplayer/settings.py
+++ myplayer/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from direnv import load
+from os import getenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x2te7vog45a6(&3rh(!glqoj^q8fca4!3k263idnt+ux1ba7mn'
+load()
+SECRET_KEY = getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
