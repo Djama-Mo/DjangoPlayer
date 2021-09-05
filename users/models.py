29010796b7310782a16b284947efb257281f304a
Djama-Mo <djamamosss@gmail.com>
2021-09-05 19:32:56 +0300

    User model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='user_pic', default='user_default')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
