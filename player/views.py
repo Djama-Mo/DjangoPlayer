e4d5b7b4ed1d7ba36e29499d149024a5ab14ef6b
Djama-Mo <djamamosss@gmail.com>
2021-09-09 19:15:13 +0300

    refactor and views of music-player templates

--- player/views.py
+++ player/views.py
@@ -12,7 +12,7 @@ class SongView(ListView):
     template_name = 'player/index.html'
     context_object_name = 'objects'
     queryset = Song.objects.select_related('genre')
-    paginate_by = 1
+    paginate_by = 30
     
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
